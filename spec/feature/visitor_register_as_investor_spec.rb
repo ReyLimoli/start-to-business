5fdb47a5fb633636a15287eecdb749f0d5d2cc2d
Carla Crude <carlacrude@gmail.com>
2018-10-13 14:19:20 -0300

    [WIP] - Cadastrar investidor com sucesso

--- spec/feature/visitor_register_as_investor_spec.rb
+++ spec/feature/visitor_register_as_investor_spec.rb
@@ -3,17 +3,18 @@ require 'rails_helper'
 feature 'visitor register as an investor' do
     scenario 'successfully' do
     
-    visit root_pah
+    visit root_path
     click_on 'Cadastrar como investidor'
-
-    fill_in 'Nome', with: 'Carla Crude', 
-    fill_in 'Documento', with: '9837927389', 
+    fill_in 'Nome', with: 'Carla Crude'
+    fill_in 'Documento', with: '9837927389'
+    fill_in 'Linkedin', with: 'linkedin' 
+    fill_in 'Data de Nascimento', with: '05-10-2018'
+    fill_in 'Valor disponível para investir', with: '6000'
     fill_in 'Email', with: 'teste@teste.com'
-    fill_in 'Senha', with: 'teste123'
-    fill_in 'Linkedin', with: 'linkedin', 
-    fill_in 'Data de Nascimento', with: '10-05-1999', 
-    fill_in 'Investimento disponível', with: '600,00'
-
-    expect(page).to have_css('h1', text: 'Investidor cadastrado com sucesso')
+    fill_in 'Senha', with: 'teste123**'
+    fill_in 'Confirme sua senha', with: 'teste123**'
+    click_on 'Cadastrar'
+  
+    expect(page).to have_content('Welcome! You have signed up successfully.')
     end
 end
