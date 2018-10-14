8052f2abdce4fd95879faa067f44558611aeb9ae
Carla Crude <carlacrude@gmail.com>
2018-10-14 00:06:29 -0300

    Ajustes bootstrap - Exbir ideias do idealizador logado

--- spec/feature/idealizer/idealizer_see_ideias_spec.rb
+++ spec/feature/idealizer/idealizer_see_ideias_spec.rb
@@ -3,19 +3,27 @@ require 'rails_helper'
 feature 'idealizer see his own ideias' do
     
     scenario 'successfully' do 
-        user = User.create!(name: 'Aparecida', email: 'user@user.com', 
-                            password:'123456', document: 123456, 
-                            linkedin: 'linkedin', birth_day: 10/05/2016)
         user_spider = User.create!(name: 'Spider', email: 'spider@user.com', 
+            password:'123456', document: 123456, 
+            linkedin: 'linkedin', birth_day: 10/05/2016)
+
+        user = User.create!(name: 'Aparecida', email: 'user2@user.com', 
                             password:'123456', document: 123456, 
                             linkedin: 'linkedin', birth_day: 10/05/2016)
-
-        ideia = Idea.create!(title: 'Suporte de geladeira', 
+        
+        suporte = Idea.create!(title: 'Suporte de geladeira', 
                              description: 'A melhor invenção', 
                              estimated_project_time: 2, 
                              initial_investment_value: 400, 
                              estimated_time_to_profit: 10,
                              user: user)
+
+        suportenovo = Idea.create!(title: 'Suportenovo', 
+                                    description: 'A melhor invenção', 
+                                    estimated_project_time: 2, 
+                                    initial_investment_value: 400, 
+                                    estimated_time_to_profit: 10,
+                                    user: user)
 
         ideia_spider = Idea.create!(title: 'Ideia de aranha', 
                             description: 'Invenção de aranha', 
@@ -31,18 +39,25 @@ feature 'idealizer see his own ideias' do
         click_on 'Entrar'
         click_on 'Minhas ideias'
 
-        expect(page).to have_css('h1', text: 'Suporte de geladeira')
+        expect(page).to have_css('h5', text: 'Suporte de geladeira')
         expect(page).to have_css('p', text: 'A melhor invenção')
-        expect(page).to have_css('p', text: '2 meses')
-        expect(page).to have_css('p', text: 'R$ 400.0')
-        expect(page).to have_css('p', text: '10 meses')
-        expect(page).to have_css('p', text: 'Aparecida')
+        expect(page).to have_css('li', text: '2 meses')
+        expect(page).to have_css('li', text: 'R$ 400.0')
+        expect(page).to have_css('li', text: '10 meses')
+        expect(page).to have_css('li', text: 'Aparecida')
 
-        expect(page).not_to have_css('h1', text: 'Ideia de aranha')
+        expect(page).to have_css('h5', text: 'Suportenovo')
+        expect(page).to have_css('p', text: 'A melhor invenção')
+        expect(page).to have_css('li', text: '2 meses')
+        expect(page).to have_css('li', text: 'R$ 400.0')
+        expect(page).to have_css('li', text: '10 meses')
+        expect(page).to have_css('li', text: 'Aparecida')
+
+        expect(page).not_to have_css('h5', text: 'Ideia de aranha')
         expect(page).not_to have_css('p', text: 'Invenção de aranha')
-        expect(page).not_to have_css('p', text: '4 meses')
-        expect(page).not_to have_css('p', text: 'R$ 500.0')
-        expect(page).not_to have_css('p', text: '20 meses')
-        expect(page).not_to have_css('p', text: 'Spider')
+        expect(page).not_to have_css('li', text: '4 meses')
+        expect(page).not_to have_css('li', text: 'R$ 500.0')
+        expect(page).not_to have_css('li', text: '20 meses')
+        expect(page).not_to have_css('li', text: 'Spider')
     end
 end
