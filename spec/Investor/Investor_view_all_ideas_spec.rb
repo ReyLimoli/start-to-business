816fd7d39d8327b5527e68152f09eac0486e1d0f
Reynaldo e Willian <reynaldo.limoli@gmail.com>
2018-10-15 18:38:51 -0300

    Investor view all ideas
      - View idea details
    
    Co-authored-by: Willian Vieira <willianmarquesv@gmail.com>
    Co-authored-by: Reynaldo Limoli <reynaldo.limoli@gmail.com>

--- spec/Investor/Investor_view_all_ideas_spec.rb
+++ spec/Investor/Investor_view_all_ideas_spec.rb
@@ -29,15 +29,12 @@ feature 'Investor_view_all_ideas' do
     within "#idea-#{idea.id}" do
       click_on 'Ver detalhes'
     end
-    expect(current_path).to eq idea_path
+    expect(current_path).to eq idea_path(idea.id)
     expect(page).to have_css('h1', text: 'Invenção da roda')
     expect(page).to have_css('p', text: 'Nova forma de utilizar a roda')
     expect(page).to have_css('p', text: '3 meses')
     expect(page).to have_css('p', text: 'R$ 10000,00')
     expect(page).to have_css('p', text: '24 meses')
-
-
-
   end
 
 end
