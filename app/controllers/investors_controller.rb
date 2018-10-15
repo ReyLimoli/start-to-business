210aed8d0ce4fbecb794e2133016133ff1778172
Carla Crude <carlacrude@gmail.com>
2018-10-15 17:23:16 -0300

    [WIP] - listar investidores

--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -2,4 +2,8 @@ class InvestorsController < ApplicationController
     def index
         @investors = User.where.not(amount_available_to_invest: nil)
     end
+
+    def show
+        @investor = User.find(params[:id])
+    end
 end
--- spec/feature/idealizer/search_investor_spec.rb
+++ spec/feature/idealizer/search_investor_spec.rb
@@ -27,5 +27,40 @@ feature 'idealizer search investor' do
     expect(page).to have_css('h5', text: 'Augusto')
     expect(page).to have_css('li', text: 'augusto@augusto.com')
     expect(page).to have_css('li', text: '600')
+
+    expect(page).not_to have_css('h5', text: 'Aparecida')
+    expect(page).not_to have_css('li', text: 'aparecida@aparecida.com')
+  end
+
+  scenario 'show investor details' do
+    
+    user_idealizer = User.create!(name: 'Aparecida', email: 'aparecida@aparecida.com', 
+        password:'123456', document: 123456, 
+        linkedin: 'linkedin', birth_day: 10/05/2016)
+
+    user_investor = User.create!(name: 'Augusto', email: 'augusto@augusto.com', 
+        password:'123456', document: 123456, 
+        linkedin: 'linkedin', birth_day: 10/05/2016, 
+        amount_available_to_invest: '600')
+
+    suporte = Idea.create!(title: 'Suporte de geladeira', 
+                            description: 'A melhor invenção', 
+                            estimated_project_time: 2, 
+                            initial_investment_value: 400, 
+                            estimated_time_to_profit: 10,
+                            user: user_idealizer)
+
+    visit root_path
+    
+    click_on 'Encontre um investidor'
+    click_on 'Augusto'
+   
+    expect(page).to have_content('Investidores')
+    expect(page).to have_css('h2', text: 'Augusto')
+    expect(page).to have_css('li', text: 'augusto@augusto.com')
+    expect(page).to have_css('li', text: '123456')
+    expect(page).to have_css('li', text: '10/05/2016')
+    expect(page).to have_css('li', text: '600')
+
   end
 end
