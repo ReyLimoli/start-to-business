d5a528aa4fafa1b1f3784e4bced94e25db856c25
Carla Crude <carlacrude@gmail.com>
2018-10-16 16:43:06 -0300

    Idealizador deve estar logado para ver investidores

--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -1,4 +1,6 @@
 class InvestorsController < ApplicationController
+    before_action :authenticate_user!
+
     def index
         @investors = User.where.not(amount_available_to_invest: nil)
     end
--- spec/feature/idealizer/search_investor_spec.rb
+++ spec/feature/idealizer/search_investor_spec.rb
@@ -59,7 +59,7 @@ feature 'idealizer search investor' do
     fill_in 'Email', with: user_idealizer.email
     fill_in 'Senha', with: user_idealizer.password
     click_on 'Entrar'
-    
+
     click_on 'Encontre um investidor'
     click_on 'Augusto'
    
@@ -68,6 +68,30 @@ feature 'idealizer search investor' do
     expect(page).to have_css('li', text: '123456')
     expect(page).to have_css('li', text: '2016-05-10')
     expect(page).to have_css('li', text: '600')
+
+  end
+
+  scenario 'should be logged in to see investors' do
+    
+    user_idealizer = User.create!(name: 'Aparecida', email: 'aparecida@aparecida.com', 
+        password:'123456', document: 123456, 
+        linkedin: 'linkedin', birth_day: '10-05-2016')
+
+    user_investor = User.create!(name: 'Augusto', email: 'augusto@augusto.com', 
+        password:'123456', document: 123456, 
+        linkedin: 'linkedin', birth_day: '10-05-2016', 
+        amount_available_to_invest: '600')
+
+    suporte = Idea.create!(title: 'Suporte de geladeira', 
+                            description: 'A melhor invenção', 
+                            estimated_project_time: 2, 
+                            initial_investment_value: 400, 
+                            estimated_time_to_profit: 10,
+                            user: user_idealizer)
+
+    visit root_path
+   
+    expect(page).not_to have_content('Encontre um investidor')
 
   end
 end
