2df6c4a978c6becd53ec485fff69af233e5731d3
Carla Crude <carlacrude@gmail.com>
2018-10-16 16:26:16 -0300

    Verificar destalhes do investidor

--- spec/feature/idealizer/search_investor_spec.rb
+++ spec/feature/idealizer/search_investor_spec.rb
@@ -5,11 +5,11 @@ feature 'idealizer search investor' do
     
     user_idealizer = User.create!(name: 'Aparecida', email: 'aparecida@aparecida.com', 
         password:'123456', document: 123456, 
-        linkedin: 'linkedin', birth_day: 10/05/2016)
+        linkedin: 'linkedin', birth_day: '10-05-2016')
 
     user_investor = User.create!(name: 'Augusto', email: 'augusto@augusto.com', 
                 password:'123456', document: 123456, 
-                linkedin: 'linkedin', birth_day: 10/05/2016, 
+                linkedin: 'linkedin', birth_day: '10-05-2016', 
                 amount_available_to_invest: '600')
 
     suporte = Idea.create!(title: 'Suporte de geladeira', 
@@ -20,6 +20,10 @@ feature 'idealizer search investor' do
                             user: user_idealizer)
 
     visit root_path
+    click_on 'Logar'
+    fill_in 'Email', with: user_idealizer.email
+    fill_in 'Senha', with: user_idealizer.password
+    click_on 'Entrar'
     
     click_on 'Encontre um investidor'
    
@@ -36,11 +40,11 @@ feature 'idealizer search investor' do
     
     user_idealizer = User.create!(name: 'Aparecida', email: 'aparecida@aparecida.com', 
         password:'123456', document: 123456, 
-        linkedin: 'linkedin', birth_day: 10/05/2016)
+        linkedin: 'linkedin', birth_day: '10-05-2016')
 
     user_investor = User.create!(name: 'Augusto', email: 'augusto@augusto.com', 
         password:'123456', document: 123456, 
-        linkedin: 'linkedin', birth_day: 10/05/2016, 
+        linkedin: 'linkedin', birth_day: '10-05-2016', 
         amount_available_to_invest: '600')
 
     suporte = Idea.create!(title: 'Suporte de geladeira', 
@@ -51,15 +55,18 @@ feature 'idealizer search investor' do
                             user: user_idealizer)
 
     visit root_path
+    click_on 'Logar'
+    fill_in 'Email', with: user_idealizer.email
+    fill_in 'Senha', with: user_idealizer.password
+    click_on 'Entrar'
     
     click_on 'Encontre um investidor'
     click_on 'Augusto'
    
-    expect(page).to have_content('Investidores')
     expect(page).to have_css('h2', text: 'Augusto')
     expect(page).to have_css('li', text: 'augusto@augusto.com')
     expect(page).to have_css('li', text: '123456')
-    expect(page).to have_css('li', text: '10/05/2016')
+    expect(page).to have_css('li', text: '2016-05-10')
     expect(page).to have_css('li', text: '600')
 
   end
