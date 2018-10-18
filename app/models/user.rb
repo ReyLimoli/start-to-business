2a3a0a39cc3c5e00f34463a7d02eb820b3102b2f
Simone e Carla <simonepereiraster@gmail.com>
2018-10-17 22:00:28 -0300

    Investidor vê suas ideias favoritas - testes OK e rubocop OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :ideas, dependent: :nullify
   has_many :proposals, dependent: :nullify
-  has_many :favorite_idea, dependent: :destroy
+  has_many :favorite_ideas, dependent: :destroy
 
   attr_accessor :user_type
 
