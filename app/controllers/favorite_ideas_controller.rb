06c5eed3941bf683f57efe016623a25aeaaea146
Simone e Willian <simonepereiraster@gmail.com>
2018-10-18 20:10:17 -0300

    Adicionado novo teste na feature.
    Co-authored-by: Simone Pereira <simonepereiraster@gmail.com>

--- app/controllers/favorite_ideas_controller.rb
+++ app/controllers/favorite_ideas_controller.rb
@@ -1,6 +1,10 @@
 class FavoriteIdeasController < ApplicationController
   before_action :authenticate_user!
   def index
-    @fav_ideas = current_user.favorite_ideas.where(active: true)
+    if current_user.investor?
+      @fav_ideas = current_user.favorite_ideas.where(active: true)
+    else
+      redirect_to root_path
+    end
   end
 end
