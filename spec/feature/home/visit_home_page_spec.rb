62624b83f213a03bfb01625b2aca772863032869
Carla Crude <carlacrude@gmail.com>
2018-10-18 10:42:04 -0300

    scenario to unfavorite ideas twice and links not show on home page to ivestor

--- spec/feature/home/visit_home_page_spec.rb
+++ spec/feature/home/visit_home_page_spec.rb
@@ -8,4 +8,20 @@ feature 'visit home page and see page details' do
     expect(page).to have_css('p', text: "Encontre investidores \
 dispostos a firmar parceria.")
   end
+
+  scenario 'successfully' do
+    investor = User.create!(name: 'Aparecida', email: 'user1234@user.com',
+                        password: '123456', document: 123_456,
+                        linkedin: 'linkedin', birth_day: 10 / 0o5 / 2016,
+                        amount_available_to_invest: '600')
+    visit root_path
+
+    click_on 'Logar'
+    fill_in 'Email', with: investor.email
+    fill_in 'Senha', with: investor.password
+    click_on 'Entrar'
+
+    expect(page).not_to have_content('Minhas ideias')
+    expect(page).not_to have_content('Tenho uma ideia')
+  end
 end
