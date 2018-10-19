70d1e215b5b33ad8e5ca04aa8863c18fa0fd1fe9
HenriqueMorato <riquemorato@gmail.com>
2018-10-19 14:15:25 -0300

    Front end

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,16 @@
 FactoryBot.define do
   factory :user do
+    sequence :email do |n|
+      "teste#{n}@email.com"
+    end
+    name { 'Joao' }
+    password { '12345678' }
+    linkedin { 'http://linkedin.com/joao' }
+    birth_day { 30.years.ago }
+    document { '100200300' }
+
+    trait :investor do
+      amount_available_to_invest { 500 }
+    end
   end
 end
