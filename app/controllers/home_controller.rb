c2a0b13825f7664fcae9e8b7a79b7f5ff7063088
Simone e Willian <simonepereiraster@gmail.com>
2018-10-18 21:14:30 -0300

    usuário logado ve apenas as suas ideias
    Co-authored-by: Simone Pereira <simonepereiraster@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,11 @@ class HomeController < ApplicationController
   def about_us; end
 
   def index
-    if user_signed_in? && !current_user.amount_available_to_invest.nil?
+    if user_signed_in? && current_user.investor?
       @ideas = Idea.all
-    elsif user_signed_in?
+    elsif user_signed_in? && current_user.idealizer?
       @ideas = current_user.ideas
+      redirect_to ideas_path
     end
   end
 end
